b374a02daa854b1f743fd76839feb1c1daee7958
Dam <asdfasdf.com>
2022-01-10 20:13:21 +0100

    commisdfa

--- Prueba2/src/Clase.java
+++ Prueba2/src/Clase.java
@@ -9,5 +9,6 @@ public class Clase {
 		System.out.println("Versión remota");
 		System.out.println("Versión remota");
 		System.out.println("Version del becario");
+		System.out.println("Version del becario");
 	}
 }
